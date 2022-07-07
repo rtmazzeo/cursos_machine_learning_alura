36a19def39ca4d31639e8faf6222039bfc353cc3
Ricardo <rtmazzeo@gmail.com>
2022-07-06 21:30:25 -0300

    Update machine_learning_01.py
    Definição das Features e da Classificação

--- machine_learning_01.py
+++ machine_learning_01.py
@@ -11,3 +11,9 @@ Original file is located at
 A ideia desse primeiro projeto é usar o machine learning para diferenciar dois animais com características diferentes: um porco e um cachorro.
 """
 
+#Será usada classificação binária: 0 = Cachorro e 1 = Porco (Features)
+#Usaremos 3 caractéristica: (Classificação)
+
+#  0 = Pelo Curto e 1 = Pelo Longo
+#  0 = Perna Longa e 1 = Perna Curta
+#  0 = não faz 'Au Au' e 1 = faz 'Au au'
