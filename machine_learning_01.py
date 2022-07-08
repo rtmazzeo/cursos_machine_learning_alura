c55ce50160b671f77c5c35b382d4980206e0f3c7
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2022-07-08 07:59:37 -0300

    Add file via upload
    Refazendo o modelo, com uma base de dados externa e maior.
    Usamos a biblioteca pandas para abrir os dados. Renomeamos as variáveis.

--- machine_learning_01.py
+++ machine_learning_01.py
@@ -70,3 +70,44 @@ from sklearn.metrics import accuracy_score
 
 taxa_de_acerto = accuracy_score(teste_y,previsoes)
 print('Taxa de Acertos: %.2f'%(taxa_de_acerto*100), '%')
+
+"""## **Projeto 2 - Classificação**
+--- 
+Utilizando uma nova fonte de dados
+
+"""
+
+import pandas as pd
+uri = 'https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv'
+dados = pd.read_csv(uri)
+
+mapa = { 
+    'home':'principal',
+    'how_it_works':'como_funciona',
+    'contact': 'contato',
+    'bought': 'comprado'}
+
+dados = dados.rename(columns = mapa)
+
+x = dados[['principal','como_funciona','contato']]
+y = dados['comprado']
+
+"""Separando o treino do teste"""
+
+dados.shape
+
+#iremos separar 75% dos dados para treinar o algoritimo
+treino_x = x[:75]
+treino_y = y[:75]
+teste_x = x[75:]
+teste_y = y[75:]
+
+print('Treinaremos com %d elementos e testaremos com %d elementos' %(len(treino_x),len(teste_x)))
+
+modelo = LinearSVC()
+modelo.fit(treino_x,treino_y)
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y,previsoes) * 100
+
+print('Taxa de Acertos: %.2f'%(acuracia), '%')
