26955b426a11934f080450d78313cd39bffbb712
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2022-07-13 09:00:26 -0300

    Add via upload
    Novo projeto - Testando duas dimensões e Criando um algoritmo de base

--- machine_learning_01.py
+++ machine_learning_01.py
@@ -129,3 +129,54 @@ previsoes = modelo.predict(teste_x)
 acuracia = accuracy_score(teste_y,previsoes) * 100
 
 print('Taxa de Acertos: %.2f'%(acuracia), '%')
+
+"""## Testando duas Dimensões
+---
+"""
+
+import pandas as pd
+uri = 'https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv'
+dados = pd.read_csv(uri)
+
+mapa = { 
+    'unfinished':'nao_finalizado',
+    'expected_hours':'horas_esperadas',
+    'price': 'preco'}
+
+dados = dados.rename(columns = mapa)
+
+dados.head()
+
+troca = {0:1,1:0}
+
+dados['finalizado'] = dados.nao_finalizado.map(troca)
+
+dados.tail(5)
+
+import seaborn as sns
+
+sns.scatterplot(x='horas_esperadas',y='preco', data=dados)
+
+import seaborn as sns
+
+sns.scatterplot(x='horas_esperadas',y='preco', hue='finalizado', data=dados)
+
+sns.relplot(x='horas_esperadas',y='preco', hue = 'finalizado', col='finalizado', data=dados)
+
+x = dados[['horas_esperadas', 'preco']]
+y = dados['finalizado']
+
+SEED = 20
+
+treino_x, teste_x,treino_y, teste_y = train_test_split(x,y,
+                                                       random_state = SEED, 
+                                                       stratify = y, # para que a proporção dos que compraram e não compraram sejam semelhantes (comparaveis)
+                                                       test_size =0.25)
+
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+print('Taxa de Acertos: %.2f'%(acuracia), '%')
+
+import numpy as np
+previsoes_de_base = np.ones(540)
+acuracia = accuracy_score(teste_y,previsoes_de_base)*100
+print('A acurácia do algoritmo de baseline: %.2f'%(acuracia), '%')
