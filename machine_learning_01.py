13378ab8bffa54b5b41eb41cfe08f4de0a04ac51
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2022-07-19 08:13:31 -0300

    Add arquivo upload - atualização modelo previsão
    Utilização de um modelo não linear (SVC) e utilização de um escalador para redefinir as escalas dos eixo de modo semelhante

--- machine_learning_01.py
+++ machine_learning_01.py
@@ -216,5 +216,84 @@ import matplotlib.pyplot as plt
 plt.contourf(xx,yy,z,alpha=0.3)
 plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y,s=1)
 
-"""Observamos que o algoritmo que estamos utilizando só é capaz de aprender uma linha reta, que não serve para classificação. Precisamos de algum modelo estimador que seja capaz de aprender um padrão mais inteligente. """
+"""Observamos que o algoritmo que estamos utilizando só é capaz de aprender uma linha reta, que não serve para classificação. Precisamos de algum modelo estimador que seja capaz de aprender um padrão mais inteligente.
+
+## Estimadores não lineares e support vector machine
+"""
+
+from sklearn.svm import SVC
+
+SEED = 5
+np.random.seed(SEED)
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,
+                                                         stratify = y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = SVC()
+modelo.fit(treino_x,treino_y)
+previsoes = modelo.predict(teste_x)
+acuracia = accuracy_score(teste_y,previsoes) * 100
+
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+print('Taxa de Acertos: %.2f'%(acuracia), '%')
+
+x_min = teste_x.horas_esperadas.min()
+x_max = teste_x.horas_esperadas.max()
+y_min = teste_x.preco.min()
+y_max = teste_x.preco.max()
+
+pixels = 100
+eixo_x = np.arange(x_min,x_max, (x_max - x_min) / pixels)
+eixo_y = np.arange(y_min,y_max, (y_max - y_min) / pixels)
+
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+pontos = np.c_[xx.ravel(), yy.ravel()]
+
+z = modelo.predict(pontos)
+z = z.reshape(xx.shape)
+
+plt.contourf(xx,yy,z,alpha=0.3)
+plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y,s=1)
+
+#Padronizar os dados em uma faixa de valor específico
+from sklearn.preprocessing import StandardScaler
+
+SEED = 5
+np.random.seed(SEED)
+raw_treino_x, raw_teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,
+                                                         stratify = y)
+
+scaler = StandardScaler()
+scaler.fit(raw_treino_x)
+treino_x = scaler.transform(raw_treino_x)
+teste_x = scaler.transform(raw_teste_x)
+
+modelo = SVC()
+modelo.fit(treino_x,treino_y)
+previsoes = modelo.predict(teste_x)
+acuracia = accuracy_score(teste_y,previsoes) * 100
+
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+print('Taxa de Acertos: %.2f'%(acuracia), '%')
+
+data_x = teste_x[:,0]
+data_y = teste_x[:,1]
+
+x_min = data_x.min()
+x_max = data_x.max()
+y_min = data_y.min()
+y_max = data_y.max()
+
+pixels = 100
+eixo_x = np.arange(x_min,x_max, (x_max - x_min) / pixels)
+eixo_y = np.arange(y_min,y_max, (y_max - y_min) / pixels)
+
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+pontos = np.c_[xx.ravel(), yy.ravel()]
+
+z = modelo.predict(pontos)
+z = z.reshape(xx.shape)
+
+plt.contourf(xx,yy,z,alpha=0.3)
+plt.scatter(data_x, data_y, c=teste_y,s=1)
 
