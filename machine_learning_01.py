4bf6f278f9fcc59e5fd8a4166891ecc3276f781e
Ricardo <96831662+rtmazzeo@users.noreply.github.com>
2022-07-19 08:14:34 -0300

    Adicionado arquivo upload - Novo Projeto
    Novo projeto de venda de carros - preparação dos dados e analise utilizando SVClinear

--- machine_learning_01.py
+++ machine_learning_01.py
@@ -297,3 +297,52 @@ z = z.reshape(xx.shape)
 plt.contourf(xx,yy,z,alpha=0.3)
 plt.scatter(data_x, data_y, c=teste_y,s=1)
 
+"""#*Novo Projeto*#
+---
+"""
+
+url = 'https://gist.githubusercontent.com/guilhermesilveira/4d1d4a16ccbf6ea4e0a64a38a24ec884/raw/afd05cb0c796d18f3f5a6537053ded308ba94bf7/car-prices.csv'
+dados = pd.read_csv(url)
+
+a_renomear = { 
+    'mileage_per_year' : 'milhas_por_ano',
+    'model_year' : 'ano_do_modelo',
+    'price' : 'preco',
+    'sold' :'vendido'}
+
+dados = dados.rename(columns=a_renomear)
+dados.head()
+
+#trocar 0 = no // 1 = yes
+
+a_trocar = {'no':0,'yes':1}
+dados.vendido = dados.vendido.map(a_trocar)
+dados.head()
+
+from datetime import datetime
+
+ano_atual = datetime.today().year
+dados['idade_do_modelo'] = ano_atual - dados.ano_do_modelo
+dados.head()
+
+dados['km_por_ano'] = dados.milhas_por_ano * 1.60934
+dados.head()
+
+dados = dados.drop(columns = ['Unnamed: 0','milhas_por_ano','ano_do_modelo'], axis=1)
+dados.head()
+
+x = dados[['preco','idade_do_modelo','km_por_ano']]
+y = dados['vendido']
+
+SEED = 5
+np.random.seed(SEED)
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,
+                                                         stratify = y)
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
+
+modelo = LinearSVC()
+modelo.fit(treino_x, treino_y)
+previsoes = modelo.predict(teste_x)
+
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print("A acurácia foi %.2f%%" % acuracia)
